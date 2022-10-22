49ca609be0dcc7963d58a5e4eba2a205cb823c2f
krishnajaya28 <116451166+Krishnajaya28@users.noreply.github.com>
2022-10-22 12:49:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,13 @@ streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.title('My Mom''s New Healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
