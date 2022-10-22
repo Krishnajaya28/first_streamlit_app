1a45aacf36f3395e956ad57b969450431f2be90d
krishnajaya28 <116451166+Krishnajaya28@users.noreply.github.com>
2022-10-22 12:47:11 -0400

    Update streamlit_app.py
    Added Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,10 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry oatmeal')
 streamlit.text('Kale, Spinach & Rocket smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
