9f8c97c0b4c10a2da53503b4a651f2ef30c83df0
krishnajaya28 <116451166+Krishnajaya28@users.noreply.github.com>
2022-10-23 13:50:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,21 +76,28 @@ streamlit.dataframe(fruits_to_show)
 #streamlit.dataframe(fruityvice_normalized)
 
 
-streamlit.header("Fruityvice Fruit Advice!")
+
 #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+#create the repeatable code block called a function)
+def get_fruityvice_data(this_fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
     #streamlit.write('The user entered ', fruit_choice)
-
+    back_from_function = get_fruityvice_data(fruit_choice)
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    streamlit.dataframe(back_from_function)
     #streamlit.text(fruityvice_response.json())
     # write your own comment -what does the next line do? 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     #write your own comment - what does this do?
-    streamlit.dataframe(fruityvice_normalized)
+    #streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
   streamlit.error()
