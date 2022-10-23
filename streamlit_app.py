4038d01f63ac0d7459b1a959532d83c8a08bb663
krishnajaya28 <116451166+Krishnajaya28@users.noreply.github.com>
2022-10-23 12:52:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,9 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
